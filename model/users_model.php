14cbc88d7eb34c14cb9df542e14db75116375ec9
Toni Fernandez <toni.fernandez@cirvianum.cat>
2024-09-26 17:00:25 +0200

    update user_model

--- model/users_model.php
+++ model/users_model.php
@@ -1,24 +1,19 @@
 <?php
-//Array d'usuaris
-$users = [
-     [
-        "id"=> 0,
-        "name"=> "Toni Fernandez",
-        "username" => "admin",
-        "password" => "P@ssw0rd",
-        "mail" => "toni.fernandez@cirvianum.cat",
-        "rol" => "admin"
-     ],
-     [
-      "id"=> 1,
-      "name"=> "Raquel Boronat",
-      "username" => "raquel",
-      "password" => "P@ssw0rd",
-      "mail" => "raquel.boronat@cirvianum.cat",
-      "rol" => "user"
-   ]
+$users = [ [
+         "id"=> 0,
+         "name"=> "Toni Fernandez",
+         "username" => "admin",
+         "password" => "P@ssw0rd",
+         "mail" => "toni.fernandez@cirvianum.cat",
+         "rol" => "admin"
+     ],[
+         "id"=> 1,
+         "name"=> "Raquel Boronat",
+         "username" => "raquel",
+         "password" => "P@ssw0rd",
+         "mail" => "raquel.boronat@cirvianum.cat",
+         "rol" => "user" ]
 ];
-
 $_SESSION['users']=$users;
 
 
