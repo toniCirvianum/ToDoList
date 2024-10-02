21f9a97cf7094f54ef5f2b43d24010a342ef4c06
Toni Fernandez <toni.fernandez@cirvianum.cat>
2024-10-02 19:32:16 +0200

    Add css local

--- templates/navbar.php
+++ templates/navbar.php
@@ -5,10 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign in</title>
+    <!--
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    -->
+    <link href="../css/bootstrap-icons.css" rel="stylesheet">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    
+    <script src="../css/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 
 </head>
 
@@ -43,8 +50,9 @@
                             <!-- editar usuaris -->
                             <a class="btn btn-info me-2" href="../view/userList_view.php"><i class="bi bi-people-fill"></i></a>
                         </li>
-                    <?php } 
-                    //botons d'usuari normal ?>
+                    <?php }
+                    //botons d'usuari normal 
+                    ?>
                     <li class="nav-item">
                         <!-- afegir toDo -->
                         <a class="btn btn-primary me-2" href="../view/toDo_view.php"><i class="bi bi-plus-square"></i></a>
